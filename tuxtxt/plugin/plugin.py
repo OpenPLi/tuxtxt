2637f9d19d8e886c8374799507f3f99524c38d6a
Littlesat <Littlesat99@yahoo.com>
2011-06-25 21:47:38 +0200

    New fix for reload picons after tuxtxt now also mute is coming back
    As I got segmentation faults while using PSignal and calling python
    functions from cpp I used a polling mechanism

--- tuxtxt/plugin/plugin.py
+++ tuxtxt/plugin/plugin.py
@@ -1,8 +1,45 @@
-from enigma import eTuxtxtApp
+from enigma import eTuxtxtApp, eTimer
+from enigma import eConsoleAppContainer, iServiceInformation, fbClass, eRCInput, eDBoxLCD
+from Screens.Screen import Screen
 from Plugins.Plugin import PluginDescriptor
+from Components.Renderer.Picon import reloadPicons
+from os import symlink, mkdir, remove, rmdir, path
+
+class ShellStarter(Screen):
+	skin = """
+		<screen position="1,1" size="1,1" title="TuxTXT" >
+                </screen>"""
+
+	def __init__(self, session, args = None):
+		self.skin = ShellStarter.skin
+		Screen.__init__(self, session)
+		self.container=eConsoleAppContainer()
+		self.container.appClosed.append(self.finished)
+		self.timer = eTimer()
+		self.timer.callback.append(self.pollUIRunning)
+		self.runapp()
+
+	def runapp(self):
+		eTuxtxtApp.getInstance().startUi()
+		print eTuxtxtApp.getInstance().getTuxtxtUIRunning()
+		self.timer.start(1000)
+
+	def pollUIRunning(self):
+		a = eTuxtxtApp.getInstance().getTuxtxtUIRunning()
+		print a
+		if eTuxtxtApp.getInstance().getTuxtxtUIRunning()[0] == 1:
+			self.timer.start(1000)
+		else:
+			self.timer.stop()
+			reloadPicons()
+			self.finished(-1)
+
+	def finished(self,retval):
+		self.close()
 
 def main(session, **kwargs):
-	eTuxtxtApp.getInstance().startUi()
+	session.open(ShellStarter)
 
 def Plugins(**kwargs):
 	return PluginDescriptor(name="TuxTXT", description="Videotext", where = PluginDescriptor.WHERE_TELETEXT, fnc=main)
+
