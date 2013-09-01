569b0945f562461458d2cd65bee085273d384f4f
Littlesat <littlesat99@yahoo.com>
2013-09-01 08:58:50 +0200

    Work-a-round for subtitles disturbing with tuxtxt screen
    It seems when subtitles are enabled the teletekst screens get disturbed.
    By disabling the subtitles before opening tuxtxt and re-enable them when
    tuxtxt is closed you do nog get disturbed tuxtxt screens anymore.
    
    Likely this is caused because of the changes in the OSD required for
    tuxtxt.

--- tuxtxt/plugin/plugin.py
+++ tuxtxt/plugin/plugin.py
@@ -1,6 +1,7 @@
 from enigma import eTuxtxtApp, getDesktop
 from Screens.Screen import Screen
 from Plugins.Plugin import PluginDescriptor
+from Screens.InfoBar import InfoBar
 
 class ShellStarter(Screen):
 	skin = """
@@ -10,6 +11,9 @@ class ShellStarter(Screen):
 	def __init__(self, session, args = None):
 		self.skin = ShellStarter.skin
 		Screen.__init__(self, session)
+		self.subtitle = InfoBar.instance.selected_subtitle
+		if self.subtitle:
+			InfoBar.instance.enableSubtitle(None)
 		eTuxtxtApp.getInstance().appClosed.get().append(self.appClosed)
 		eTuxtxtApp.getInstance().startUi()
 
@@ -18,6 +22,8 @@ class ShellStarter(Screen):
 		#force redraw
 		dsk = getDesktop(0)
 		dsk.resize(dsk.size())
+		if self.subtitle:
+			InfoBar.instance.enableSubtitle(self.subtitle)
 		self.close()
 
 def main(session, **kwargs):
